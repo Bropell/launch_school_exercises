54bec611f429a78856412833b70d6fc1ce70257b
Brandon Ropell <brandon.ropell@gmail.com>
2020-07-17 12:54:16 -0400

    Added exercise 3 and question prompts to exercises 1 and 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,3 +1,9 @@
+#Use modulo, division or both operators
+#to take a 4-digit number and find the 
+#digit in the 1)thousands place
+#2)hundreds place 3)tens place and 
+#4)ones place
+
 thousands_digit = 2965 / 1000
 puts thousands_digit
 
