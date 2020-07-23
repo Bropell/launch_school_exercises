4209ee7782b75089808059e20806ccbd304d8600
Brandon Ropell <brandon.ropell@gmail.com>
2020-07-23 15:40:16 -0400

    Added exercise 1 and fibonacci.rb

---
+++ loops_and_iterators_exercises/exercise1.rb
@@ -0,0 +1,9 @@
+#What does the 'each' method in the following
+#program return after it is finished executing?
+
+x = [1, 2, 3, 4, 5]
+x.each do |a|
+  a + 1 
+end
+
+#Program should return the original array
