a5fedfb0a32f7ddbedb7103d97fefdc1939f1771
Brandon Ropell <34928829+Bropell@users.noreply.github.com>
2020-07-23 01:34:20 -0400

    Delete exercise4.rb

--- exercise4.rb
+++
@@ -1,8 +0,0 @@
-#Use the dates from the previous exercise
-#and store them in an array. Then output
-#the same thing as exercise 3.
-
-dates = [2001, 1999, 1975, 2007]
-dates.each do|year|
-  puts year
-end
