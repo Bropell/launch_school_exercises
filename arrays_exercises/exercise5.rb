aa74b5955f3aa7b223ca9089d1abe1119ac8b15a
Brandon Ropell <brandon.ropell@gmail.com>
2020-07-26 17:38:37 -0400

    Added exercises 4-7

---
+++ arrays_exercises/exercise5.rb
@@ -0,0 +1,8 @@
+#What is the value of a, b, and
+#c in the following program?
+
+string = "Welcome to America!"
+
+a = string[6]   #a should have a value of "e"
+b = string[11]  #b should have a value of "A"
+c = string[19]  #c should have a value of nil
