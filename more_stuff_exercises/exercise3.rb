75b2653816d60ba54376033782f100b677355241
Brandon Ropell <34928829+Bropell@users.noreply.github.com>
2020-07-29 12:56:55 -0400

    Delete exercise3.rb

--- more_stuff_exercises/exercise3.rb
+++
@@ -1,7 +0,0 @@
-#What is exception handling and what 
-#problem does it solve?
-
-#Exception handling is a specific process that
-#deals ith errors in a manageable and predictable
-#way. This solves the problem of the program closing
-#when an error occurs by changing the flow of control.
