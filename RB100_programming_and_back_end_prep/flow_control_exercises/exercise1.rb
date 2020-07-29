8c8f738f30647445bcbe2431af674e9be81b8947
Brandon Ropell <brandon.ropell@gmail.com>
2020-07-29 12:30:20 -0400

    Moved flow_control_exercises

---
+++ RB100_programming_and_back_end_prep/flow_control_exercises/exercise1.rb
@@ -0,0 +1,13 @@
+#Write down whether the following expressions
+#return true or false.
+
+(32 * 4) >= 129 #false
+
+false != !true  #false 
+
+true == 4       #false
+
+false == (847 == '874') #true
+
+(!true || (!(100/5) == 20) || ((328 / 4) == 82)) || false #true
+
---
+++ RB100_programming_and_back_end_prep/flow_control_exercises/exercise4.rb
@@ -0,0 +1,29 @@
+#What will each block of code below print
+#to the screen? 
+
+#Snippet 1
+"4" == 4 ? puts("TRUE") : puts("FALSE")
+#Should print FALSE
+
+#Snippet 2
+x =2
+if ((x * 3) / 2) == (4 + 4 -x - 3)
+    puts "Did you get it right?"
+else
+    puts "Did you?"
+end
+#Should print Did you get it right?
+
+#Snippet 3
+y = 9
+x =10
+if (x + 1) <= (y)
+    puts "Alright."
+elsif (x + 1) >= (y)
+    puts "Alright now!"
+elsif (y + 1) == x
+    puts "ALRIGHT NOW!"
+else
+    puts "Alrighty!"
+end
+#Should print Alright now!
