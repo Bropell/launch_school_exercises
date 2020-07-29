890ac00d17a2cf05f7543d05dbae487ff84e2a80
Brandon Ropell <brandon.ropell@gmail.com>
2020-07-29 15:08:40 -0400

    Added exercise 4 and updated exercise 3

--- RB100_programming_and_back_end_prep/final_exercises/exercise3.rb
+++ RB100_programming_and_back_end_prep/final_exercises/exercise3.rb
@@ -5,4 +5,4 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 odd_array = array.select {|n| n.odd? }
-puts odd_array
+p odd_array
---
+++ RB100_programming_and_back_end_prep/final_exercises/exercise4.rb
@@ -0,0 +1,8 @@
+#Append 11 to the end of the original array. 
+#Prepend 0 to the beginning.
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+array.append(11)
+array.unshift(0)
+p array 
