187675973be845810b570fe89e5cff060994b476
Brandon Ropell <34928829+Bropell@users.noreply.github.com>
2020-07-29 12:56:41 -0400

    Delete exception_example.rb

--- more_stuff_exercises/exception_example.rb
+++
@@ -1,9 +0,0 @@
-names = ['bob', 'joe', 'steve', nil, 'frank']
-
-names.each do |name|
-  begin 
-    puts "#{name}'s name has #{name.length} letters in it."
-  rescue
-    puts "Something went wrong!"
-  end
-end
