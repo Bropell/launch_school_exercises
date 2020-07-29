4c421b1155b0407080c2ae4689dc796532c0a1c2
Brandon Ropell <34928829+Bropell@users.noreply.github.com>
2020-07-29 12:44:31 -0400

    Delete exercise7.rb

--- arrays_exercises/exercise7.rb
+++
@@ -1,6 +0,0 @@
-#Use the each_with_index method to iterate
-#through an array of your creation that
-#prints each index and value of the array
-
-array = [23, 4, "Brandon", 69, 4.78]
-array.each_with_index { |value, index| puts "#{index}. #{value}"}
