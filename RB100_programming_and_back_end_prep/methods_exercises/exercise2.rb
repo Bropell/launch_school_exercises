a659aa2ebfaf81da097ffc4e7e81889fae9ff24a
Brandon Ropell <brandon.ropell@gmail.com>
2020-07-29 12:41:52 -0400

    Moved methods_exercises

---
+++ RB100_programming_and_back_end_prep/methods_exercises/exercise2.rb
@@ -0,0 +1,12 @@
+#What do the following expressions 
+#evaluate to?
+
+#x = 2                        => 2
+
+#puts x = 2                   => nil
+
+#p name = "Joe"               => "Joe"
+
+#four = "four"                => "four"
+
+#print something = "nothing"  => nil
