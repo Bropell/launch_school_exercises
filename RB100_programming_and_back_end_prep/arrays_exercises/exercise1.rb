5fae7d35be1346dbfc4e0401a07e030191e3273f
Brandon Ropell <brandon.ropell@gmail.com>
2020-07-29 12:19:37 -0400

    Moved arrays_exercises

---
+++ RB100_programming_and_back_end_prep/arrays_exercises/exercise1.rb
@@ -0,0 +1,10 @@
+#Below we have given you an array and 
+#a number. Write a program that checks
+#to see if the number appears in the array
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if arr.include?(number)
+    puts "#{number} appears in the array"
+end
---
+++ RB100_programming_and_back_end_prep/arrays_exercises/exercise3.rb
@@ -0,0 +1,6 @@
+#How do you return the word "example"
+#from the following array?
+
+arr = [["test", "hello", "world"], ["example", "mem"]]
+
+arr.last.first
